4f540309139afe55733f488ca9e63020b8c10afb
tmcarvalho <taniammc.94@gmail.com>
2023-10-10 22:11:06 +0100

    remove duplicates

--- code/modeling/modelingHB.py
+++ code/modeling/modelingHB.py
@@ -7,12 +7,9 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import SGDClassifier
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.linear_model import SGDClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import make_scorer, roc_auc_score
 from sklearn.experimental import enable_halving_search_cv #noqa
-from sklearn.model_selection import HalvingRandomSearchCV, RepeatedStratifiedKFold
 from sklearn.model_selection import HalvingRandomSearchCV, RepeatedStratifiedKFold
 from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
@@ -59,18 +56,8 @@ def evaluate_model_hb(x_train, x_test, y_train, y_test):
     # param1['classifier__max_depth'] = [6, 8, 10, 12]
     # param1['classifier__learning_rate'] = [0.01, 0.1, 0.3, 0.5]
     # param1['classifier'] = [xgb]
-    # param1 = {}
-    # param1['classifier__n_estimators'] = [100, 250, 500, 750, 1000]
-    # param1['classifier__max_depth'] = [6, 8, 10, 12]
-    # param1['classifier__learning_rate'] = [0.01, 0.1, 0.3, 0.5]
-    # param1['classifier'] = [xgb]
 
     param2 = {}
-    param2['classifier__alpha'] = [1e-3, 1e-4, 1e-5]
-    param2['classifier__max_iter'] = [1000, 10000, 1000000]
-    param2['classifier__eta0'] = [0, 0.01, 0.1, 0.5, 1]
-    param2['classifier'] = [sdg]
-    
     param2['classifier__alpha'] = [1e-3, 1e-4, 1e-5]
     param2['classifier__max_iter'] = [1000, 10000, 1000000]
     param2['classifier__eta0'] = [0, 0.01, 0.1, 0.5, 1]
@@ -81,15 +68,10 @@ def evaluate_model_hb(x_train, x_test, y_train, y_test):
     param3['classifier__max_depth'] = [6, 8, 10, 12]
     param3['classifier__learning_rate'] = [0.01, 0.1, 0.3, 0.5]
     param3['classifier'] = [grb]
-    param3['classifier__n_estimators'] = [100, 250, 500, 750, 1000]
-    param3['classifier__max_depth'] = [6, 8, 10, 12]
-    param3['classifier__learning_rate'] = [0.01, 0.1, 0.3, 0.5]
-    param3['classifier'] = [grb]
 
     param4 = {}
     param4['classifier__hidden_layer_sizes'] = [[n_feat // 2], [int(n_feat * (2 / 3))], [n_feat]]
     param4['classifier__alpha'] = [1e-2, 1e-3, 1e-4]
-    param4['classifier__max_iter'] = [1000, 10000, 1000000]
     param4['classifier__max_iter'] = [1000, 10000, 1000000]
     param4['classifier'] = [nnet]
 
@@ -98,11 +80,8 @@ def evaluate_model_hb(x_train, x_test, y_train, y_test):
 
     pipeline = Pipeline([('classifier', sdg)])
     params = [param2, param3, param4]
-    pipeline = Pipeline([('classifier', sdg)])
-    params = [param2, param3, param4]
 
     print("Start modeling with CV")
-    training = time.time()
     training = time.time()
 
     # Train the grid search model
@@ -115,7 +94,6 @@ def evaluate_model_hb(x_train, x_test, y_train, y_test):
         n_jobs=-1).fit(x_train, y_train)
 
     print(f'It takes {(time.time() - training)/60} minutes')
-    print(f'It takes {(time.time() - training)/60} minutes')
 
     score_cv = {
     'params':[], 'model':[], 'test_roc_auc':[]
@@ -126,15 +104,10 @@ def evaluate_model_hb(x_train, x_test, y_train, y_test):
     # validation['model'] = validation['model'].apply(lambda x: 'XGBoost' if 'XGB' in str(x) else x)
     validation['model'] = validation['model'].apply(lambda x: 'Stochastic Gradient Descent' if 'SGDClassifier' in str(x) else x)
     validation['model'] = validation['model'].apply(lambda x: 'Gradient Boosting' if 'GradientBoostingClassifier' in str(x) else x)
-    # validation['model'] = validation['model'].apply(lambda x: 'XGBoost' if 'XGB' in str(x) else x)
-    validation['model'] = validation['model'].apply(lambda x: 'Stochastic Gradient Descent' if 'SGDClassifier' in str(x) else x)
-    validation['model'] = validation['model'].apply(lambda x: 'Gradient Boosting' if 'GradientBoostingClassifier' in str(x) else x)
     validation['model'] = validation['model'].apply(lambda x: 'Neural Network' if 'MLPClassifier' in str(x) else x)
-    validation['time'] = (time.time() - training)/60
     validation['time'] = (time.time() - training)/60
 
     print("Start modeling in out of sample")
-    outofsample = time.time()
     outofsample = time.time()
     for i in range(len(validation)):
         # set each model for prediction on test
@@ -143,7 +116,6 @@ def evaluate_model_hb(x_train, x_test, y_train, y_test):
         score_cv['params'].append(str(grid.cv_results_['params'][i]))
         score_cv['model'].append(validation.loc[i, 'model'])
         score_cv['test_roc_auc'].append(roc_auc_score(y_test, clf))
-        score_cv['time'] = (time.time() - outofsample)/60
         score_cv['time'] = (time.time() - outofsample)/60
 
     score_cv = pd.DataFrame(score_cv)

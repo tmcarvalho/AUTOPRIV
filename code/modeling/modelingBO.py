ab04acbfdcb0b7b6b0bf7ccd4d48d93d1db266af
tmcarvalho <taniammc.94@gmail.com>
2023-10-11 09:57:21 +0000

    early sotpping algorithms

--- code/modeling/modelingBO.py
+++ code/modeling/modelingBO.py
@@ -5,8 +5,8 @@ import warnings
 import time
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import SGDClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import make_scorer, roc_auc_score
 from skopt import BayesSearchCV
@@ -33,61 +33,59 @@ def evaluate_model_bo(x_train, x_test, y_train, y_test):
     seed = np.random.seed(1234)
 
     # initiate models
-    rfc = RandomForestClassifier(random_state=seed)
-    booster = XGBClassifier(
-            objective='binary:logistic',
-            eval_metric='logloss',
-            use_label_encoder=False,
-            random_state=seed, early_stopping_rounds=10)
+    grb = GradientBoostingClassifier(loss='log_loss',
+                                    n_iter_no_change=10,
+                                    random_state=seed)
 
-    reg = LogisticRegression(random_state=seed)
-    nnet = MLPClassifier(random_state=seed)
+    sdg = SGDClassifier(early_stopping=True,
+                        n_iter_no_change=10,
+                        random_state=seed)
+
+    nnet = MLPClassifier(early_stopping=True,
+                         n_iter_no_change=10,
+                        random_state=seed)
 
     n_feat = x_train.shape[1]
 
     # set parameterisation
-    param1 = {}
-    param1['classifier__n_estimators'] = Integer(100, 500)
-    param1['classifier__max_depth'] = Integer(4, 10)
-    param1['classifier'] = [rfc]
-
     param2 = {}
-    param2['classifier__n_estimators'] = Integer(100, 500)
-    param2['classifier__max_depth'] = Integer(4, 10)
-    param2['classifier__learning_rate'] = Real(1e-2, 1e-1)
-    param2['classifier'] = [booster]
-
+    param2['classifier__alpha'] = Real(1e-5, 1e-3)
+    param2['classifier__max_iter'] = Integer(1000, 1000000)
+    param2['classifier__eta0'] = Integer(0, 1)
+    param2['classifier'] = [sdg]
+    
     param3 = {}
-    param3['classifier__C'] = Real(1.e-4, 1.e4)
-    param3['classifier__max_iter'] = Integer(1000000, 100000000)
-    param3['classifier'] = [reg]
+    param3['classifier__n_estimators'] = Integer(100, 1000)
+    param3['classifier__max_depth'] = Integer(6, 12)
+    param3['classifier__learning_rate'] = Real(0.01, 0.5)
+    param3['classifier'] = [grb]
 
     param4 = {}
     param4['classifier__hidden_layer_sizes'] = Integer(int(n_feat // 2), n_feat)
     param4['classifier__alpha'] = Real(1e-4, 1e-2)
-    param4['classifier__max_iter'] = Integer(10000, 100000)
+    param4['classifier__max_iter'] = Integer(1000, 1000000)
     param4['classifier'] = [nnet]
 
     # define metric functions -- doens't accept multi measures
-    scoring = make_scorer(roc_auc_score, max_fpr=0.001, needs_proba=True)
+    scoring = make_scorer(roc_auc_score, max_fpr=0.001, needs_proba=False)
 
-    pipeline = Pipeline([('classifier', rfc)])
-    params = [param1, param2, param3, param4]
+    pipeline = Pipeline([('classifier', sdg)])
+    params = [param2, param3, param4]
 
     print("Start modeling with CV")
-    start = time.time()
+    training = time.time()
 
     # Train the grid search model
     grid = BayesSearchCV(
         pipeline,
         search_spaces=params,
-        n_iter=32,
+        n_iter=50,
         cv=RepeatedStratifiedKFold(n_splits=5, n_repeats=2, random_state=1),
         scoring=scoring,
         return_train_score=True,
         n_jobs=-1).fit(x_train, y_train)
 
-    print(f'It takes {(time.time() - start)/60} minutes')
+    print(f'It takes {(time.time() - training)/60} minutes')
 
     score_cv = {
     'params':[], 'model':[], 'test_roc_auc':[]
@@ -95,14 +93,14 @@ def evaluate_model_bo(x_train, x_test, y_train, y_test):
     # Store results from grid search
     validation = pd.DataFrame(grid.cv_results_)
     validation['model'] = validation['param_classifier']
-    validation['model'] = validation['model'].apply(lambda x: 'Random Forest' if 'RandomForest' in str(x) else x)
-    validation['model'] = validation['model'].apply(lambda x: 'XGBoost' if 'XGB' in str(x) else x)
-    validation['model'] = validation['model'].apply(lambda x: 'Logistic Regression' if 'Logistic' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'Stochastic Gradient Descent' if 'SGDClassifier' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'Gradient Boosting' if 'GradientBoostingClassifier' in str(x) else x)
     validation['model'] = validation['model'].apply(lambda x: 'Neural Network' if 'MLPClassifier' in str(x) else x)
-    validation['time'] = (time.time() - start)/60
+    validation['time'] = (time.time() - training)/60
 
     print("Start modeling in out of sample")
 
+    outofsample = time.time()
     for i in range(len(validation)):
         # set each model for prediction on test
         clf_best = grid.best_estimator_.set_params(**grid.cv_results_['params'][i]).fit(x_train, y_train)
@@ -110,7 +108,7 @@ def evaluate_model_bo(x_train, x_test, y_train, y_test):
         score_cv['params'].append(str(grid.cv_results_['params'][i]))
         score_cv['model'].append(validation.loc[i, 'model'])
         score_cv['test_roc_auc'].append(roc_auc_score(y_test, clf))
-
+        score_cv['time'] = (time.time() - outofsample)/60
     score_cv = pd.DataFrame(score_cv)
 
     return [validation, score_cv]

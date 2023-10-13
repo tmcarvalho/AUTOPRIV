8238d7feb2bad3eb1e4e1010dbc26d979977b67a
tmcarvalho <taniammc.94@gmail.com>
2023-10-13 20:33:59 +0000

    use best model in out of sample

--- code/modeling/modelingBO.py
+++ code/modeling/modelingBO.py
@@ -14,46 +14,9 @@ from skopt import BayesSearchCV
 from skopt.space import Real, Integer
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.pipeline import Pipeline
-import concurrent.futures
-import multiprocessing
 
 warnings.filterwarnings(action='ignore', category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
-
-# %% parallelize the code for testing results
-def outofsample_worker(i, x_train, x_test, y_train, y_test, grid, validation):
-    score_cv = {'params': [], 'model': [], 'test_roc_auc': []}
-    # set each model for prediction on test
-    clf_best = grid.best_estimator_.set_params(**grid.cv_results_['params'][i]).fit(x_train, y_train)
-    clf = clf_best.predict(x_test)
-    score_cv['params'].append(str(grid.cv_results_['params'][i]))
-    score_cv['model'].append(validation.loc[i, 'model'])
-    score_cv['test_roc_auc'].append(roc_auc_score(y_test, clf))
-
-    return score_cv
-
-def outofsample(x_train, x_test, y_train, y_test, grid, validation):
-    num_workers = multiprocessing.cpu_count()
-    outofsample_start = time.time()
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=num_workers) as executor:
-        futures = [executor.submit(outofsample_worker, i, x_train, x_test, y_train, y_test, grid, validation) for i in range(len(validation))]
-        concurrent.futures.wait(futures)
-
-    score_cv = {
-        'params': [],
-        'model': [],
-        'test_roc_auc': [],
-        'time': [],
-    }
-    for result in futures:
-        result_cv = result.result()
-        score_cv['params'].extend(result_cv['params'])
-        score_cv['model'].extend(result_cv['model'])
-        score_cv['test_roc_auc'].extend(result_cv['test_roc_auc'])
-        score_cv['time'] = (time.time() - outofsample_start) / 60
-    
-    return pd.DataFrame(score_cv)
 
 # evaluate a model
 def evaluate_model_bo(x_train, x_test, y_train, y_test):
@@ -129,7 +92,7 @@ def evaluate_model_bo(x_train, x_test, y_train, y_test):
     grid = BayesSearchCV(
         pipeline,
         search_spaces=params,
-        n_iter=30,
+        n_iter=50,
         cv=RepeatedStratifiedKFold(n_splits=5, n_repeats=2, random_state=1),
         scoring=scoring,
         return_train_score=True,
@@ -140,15 +103,31 @@ def evaluate_model_bo(x_train, x_test, y_train, y_test):
     # Store results from grid search
     validation = pd.DataFrame(grid.cv_results_)
     validation['model'] = validation['param_classifier']
-    validation['model'] = validation['model'].apply(lambda x: 'XGBoost' if 'XGB' in str(x) else x)
-    validation['model'] = validation['model'].apply(lambda x: 'Stochastic Gradient Descent' if 'SGDClassifier' in str(x) else x)
-    validation['model'] = validation['model'].apply(lambda x: 'Gradient Boosting' if 'GradientBoostingClassifier' in str(x) else x)
-    validation['model'] = validation['model'].apply(lambda x: 'Neural Network' if 'MLPClassifier' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'XGBClassifier' if 'XGBClassifier' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'SGDClassifier' if 'SGDClassifier' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'GradientBoostingClassifier' if 'GradientBoostingClassifier' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'MLPClassifier' if 'MLPClassifier' in str(x) else x)
     validation['time'] = (time.time() - training)/60
 
     print("Start modeling in out of sample")
 
-    # Call the outofsample function
-    score_outofsample = outofsample(x_train, x_test, y_train, y_test, grid, validation)
+    score_cv = {
+    'params':[], 'model':[], 'test_roc_auc_estimated':[], 'test_roc_auc': [], 'opt_type': [],
+    }
 
-    return [validation, score_outofsample]
+    # use the best estimated hyperparameter
+    grid_res = grid.predict(x_test)
+
+    # retrain the best estimator using all training data
+    clf_best = grid.best_estimator_.fit(x_train, y_train)
+    clf = clf_best.predict(x_test)
+
+    score_cv['params'].append(str(grid.best_params_))
+    score_cv['model'] = type(grid.best_estimator_.steps[-1][1]).__name__
+    score_cv['test_roc_auc_estimated'].append(roc_auc_score(y_test, grid_res))
+    score_cv['test_roc_auc'].append(roc_auc_score(y_test, clf))
+    score_cv['opt_type'].append("Bayes")
+    
+    score_cv = pd.DataFrame(score_cv)
+
+    return [validation, score_cv]

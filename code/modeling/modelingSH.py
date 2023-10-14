0466f6636f431c2d28340f87f72f6068b08c676f
tmcarvalho <taniammc.94@gmail.com>
2023-10-14 07:42:24 +0000

    add XGB and test with the best CV

--- code/modeling/modelingSH.py
+++ code/modeling/modelingSH.py
@@ -12,6 +12,7 @@ from sklearn.metrics import make_scorer, roc_auc_score
 from sklearn.experimental import enable_halving_search_cv #noqa
 from sklearn.model_selection import HalvingGridSearchCV, RepeatedStratifiedKFold
 from sklearn.pipeline import Pipeline
+from xgboost import XGBClassifier
 
 warnings.filterwarnings(action='ignore', category=FutureWarning)
 
@@ -31,6 +32,13 @@ def evaluate_model_sh(x_train, x_test, y_train, y_test):
     seed = np.random.seed(1234)
 
     # initiate models
+    xgb = XGBClassifier(
+            objective='binary:logistic',
+            eval_metric='logloss',
+            use_label_encoder=False,
+            early_stopping_rounds=10,
+            random_state=seed)
+    
     grb = GradientBoostingClassifier(loss='log_loss',
                                     n_iter_no_change=10,
                                     random_state=seed)
@@ -46,6 +54,12 @@ def evaluate_model_sh(x_train, x_test, y_train, y_test):
     n_feat = x_train.shape[1]
 
     # set parameterisation
+    param1 = {}
+    param1['classifier__n_estimators'] = [100, 250, 500, 750, 1000]
+    param1['classifier__max_depth'] = [6, 8, 10, 12]
+    param1['classifier__learning_rate'] = [0.01, 0.1, 0.3, 0.5]
+    param1['classifier'] = [xgb]
+
     param2 = {}
     param2['classifier__alpha'] = [1e-5, 1e-4, 1e-3]
     param2['classifier__max_iter'] = [1000, 10000, 1000000]
@@ -67,8 +81,8 @@ def evaluate_model_sh(x_train, x_test, y_train, y_test):
     # define metric functions -- doens't accept multi measures
     scoring = make_scorer(roc_auc_score, max_fpr=0.001, needs_proba=False)
 
-    pipeline = Pipeline([('classifier', sdg)])
-    params = [param2, param3, param4]
+    pipeline = Pipeline([('classifier', xgb)])
+    params = [param1, param2, param3, param4]
 
     print("Start modeling with CV")
     training = time.time()
@@ -84,29 +98,34 @@ def evaluate_model_sh(x_train, x_test, y_train, y_test):
 
     print(f'It takes {(time.time() - training)/60} minutes')
 
-    score_cv = {
-    'params':[], 'model':[], 'test_roc_auc':[]
-    }
     # Store results from grid search
     validation = pd.DataFrame(grid.cv_results_)
     validation['model'] = validation['param_classifier']
-    validation['model'] = validation['model'].apply(lambda x: 'Stochastic Gradient Descent' if 'SGDClassifier' in str(x) else x)
-    validation['model'] = validation['model'].apply(lambda x: 'Gradient Boosting' if 'GradientBoostingClassifier' in str(x) else x)
-    validation['model'] = validation['model'].apply(lambda x: 'Neural Network' if 'MLPClassifier' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'XGBClassifier' if 'XGBClassifier' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'SGDClassifier' if 'SGDClassifier' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'GradientBoostingClassifier' if 'GradientBoostingClassifier' in str(x) else x)
+    validation['model'] = validation['model'].apply(lambda x: 'MLPClassifier' if 'MLPClassifier' in str(x) else x)
     validation['time'] = (time.time() - training)/60
 
     print("Start modeling in out of sample")
 
-    outofsample = time.time()
-    for i in range(len(validation)):
-        # set each model for prediction on test
-        clf_best = grid.best_estimator_.set_params(**grid.cv_results_['params'][i]).fit(x_train, y_train)
-        clf = clf_best.predict(x_test)
-        score_cv['params'].append(str(grid.cv_results_['params'][i]))
-        score_cv['model'].append(validation.loc[i, 'model'])
-        score_cv['test_roc_auc'].append(roc_auc_score(y_test, clf))
-        score_cv['time'] = (time.time() - outofsample)/60
+    score_cv = {
+    'params':[], 'model':[], 'test_roc_auc_estimated':[], 'test_roc_auc': [], 'opt_type': [],
+    }
 
+    # use the best estimated hyperparameter
+    grid_res = grid.predict(x_test)
+
+    # retrain the best estimator using all training data
+    clf_best = grid.best_estimator_.fit(x_train, y_train)
+    clf = clf_best.predict(x_test)
+
+    score_cv['params'].append(str(grid.best_params_))
+    score_cv['model'] = type(grid.best_estimator_.steps[-1][1]).__name__
+    score_cv['test_roc_auc_estimated'].append(roc_auc_score(y_test, grid_res))
+    score_cv['test_roc_auc'].append(roc_auc_score(y_test, clf))
+    score_cv['opt_type'].append("Halving")
+    
     score_cv = pd.DataFrame(score_cv)
 
     return [validation, score_cv]

a25f6b782725645f5b349735bc069180258d4ada
tmcarvalho <taniammc.94@gmail.com>
2023-11-12 15:13:42 +0000

    run synthpop

--- code/transformations/dpart_.py
+++ code/transformations/dpart_.py
@@ -101,7 +101,7 @@ def synt_dpart(original_folder, file, technique):
 # %%
 original_folder = '../../data/original'
 _, _, input_files = next(walk(f'{original_folder}'))
-technique = 'independent'
+technique = 'synthpop'
 not_considered_files = [0,1,3,13,23,28,34,36,40,48,54,66,87]
 for idx,file in enumerate(input_files):
     if int(file.split(".csv")[0]) not in not_considered_files:

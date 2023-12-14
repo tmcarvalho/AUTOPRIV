ad51c4b7df2fdd4bf9201cae9d5bae5290eac83d
tmcarvalho <taniammc.94@gmail.com>
2023-12-14 19:07:05 +0000

    include transformations parameters

--- code/metafeatures.py
+++ code/metafeatures.py
@@ -2,7 +2,7 @@
 from pymfe.mfe import MFE
 import pandas as pd
 
-#%%
+# %%
 def meta_features(file):
     if any(word in file for word in deepl):
         data = pd.read_csv(f'../data/deep_learningk2/{file}')
@@ -27,31 +27,60 @@ def meta_features(file):
     return ftdf
 
 # %%
-performance_results = pd.read_csv('../output_analysis/resultsCV.csv')
+performance_results = pd.read_csv('../output_analysis/results_test.csv')
 linkability_results = pd.read_csv('../output/anonymeterk2.csv')
 deepl = ['TVAE', 'CopulaGAN', 'CTGAN']
-city = ['PATEGAN', 'DPGAN']
+city = ['pategan', 'dpgan']
 all_metaft = []
 c=0
 for idx in performance_results.index:
     if performance_results.opt_type[idx] == 'Hyperband':
-        print(performance_results.ds_complete[idx])
+        # print(performance_results.ds_complete[idx])
         metaft = meta_features(performance_results.ds_complete[idx])
 
         linkability_file = linkability_results.loc[(linkability_results.ds_complete == performance_results.ds_complete[idx])].reset_index(drop=True)
         metaft['linkability_score'] = linkability_file.value.iloc[0]
-        metaft['mean_test_score'] = performance_results.mean_test_score[idx]
+        metaft['test_roc_auc'] = performance_results.test_roc_auc[idx]
         metaft['ds_complete'] = performance_results.ds_complete[idx]
     
         # concat each meta feature result
         if c == 0:
             all_metaft = metaft
             c += 1
-        else:  
+        else:
             all_metaft = pd.concat([all_metaft, metaft])
-        
+  
 # %%
-all_metaft.to_csv('../output_analysis/metaft.csv', index=False)
+metaft_df = all_metaft.copy().reset_index(drop=True)
+# %%
+for idx in metaft_df.index:
+    technique = metaft_df.ds_complete[idx].split('_')[1]
+    metaft_df.at[idx, 'technique'] = technique
+    
+    if technique in (deepl + city):
+        print(''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[2])))
+        metaft_df.at[idx, 'QI'] = ''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[2]))
+        metaft_df.at[idx, 'epochs'] = ''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[3]))
+        metaft_df.at[idx, 'batch'] = ''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[4].split('.')[0]))
+        
+        if technique in city:
+            metaft_df.at[idx, 'epsilon'] = ''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[5].split('.')[0]))
+    
+    if 'transf' in metaft_df.ds_complete[idx]:
+        metaft_df.at[idx, 'n_transf'] = ''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[1]))
+        metaft_df.at[idx, 'QI'] = ''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[2].split('.')[0]))
+    
+    if 'privateSMOTE' in metaft_df.ds_complete[idx]:
+        metaft_df.at[idx, 'QI'] = ''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[2]))
+        metaft_df.at[idx, 'knn'] = ''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[3]))
+        metaft_df.at[idx, 'per'] = ''.join(filter(str.isdigit, metaft_df.ds_complete[idx].split('_')[4].split('.')[0]))
+
+# %% change columns positions
+end_cols = ['linkability_score', 'test_roc_auc'] 
+other_cols = [col for col in metaft_df.columns if col not in end_cols]
+metaft_df = metaft_df[other_cols + end_cols]
+# %%
+metaft_df.to_csv('../output_analysis/metaft.csv', index=False)
 
 # %%
 
@@ -61,5 +90,3 @@ all_metaft.to_csv('../output_analysis/metaft.csv', index=False)
 # print(len(allft[0]))
 # allftdf = pd.DataFrame(allft[1:], columns=allft[0])
 # allftdf.to_csv('output/allmetaft.csv', index=False)
-
-

71f727afcf55c9c516cd122e8c5af8bad50abbc4
tmcarvalho <taniammc.94@gmail.com>
2023-12-14 19:06:40 +0000

    data dimension

--- code/cenas.py
+++ code/cenas.py
@@ -1,5 +1,7 @@
 # %%
 from os import walk
+import pandas as pd
+# %%
 _,_,result_files = next(walk('../data/synthcityk2'))
 # %%
 ds = [file.split('_')[0] for file in result_files]
@@ -7,9 +9,17 @@ ds = [file.split('_')[0] for file in result_files]
 from collections import Counter
 x = Counter([element for element in ds])
 # %%
-_,_,files = next(walk('../data/PrivateSMOTEk2'))
+_,_,files = next(walk('../data/deep_learningk2'))
+for file in files:
+    dsss = pd.read_csv(f'../data/deep_learningk2/{file}')
+    print(dsss.shape, file)
+    print(dsss.shape[0]*0.8)
+# %%
+# %%
+_,_,files = next(walk('../data/original'))
 import pandas as pd
 for file in files:
-    dss = pd.read_csv(f'../data/PrivateSMOTEk2/{file}')
+    dss = pd.read_csv(f'../data/original/{file}')
     print(dss.shape, file)
+    print(dss.shape[0]*0.8)
 # %%

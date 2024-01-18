a5e9107c6830c36bdc17f3386dbf56dc6859dab3
tmcarvalho <taniammc.94@gmail.com>
2024-01-18 13:13:38 +0000

    k3

--- code/transformations/city.py
+++ code/transformations/city.py
@@ -9,22 +9,11 @@ from synthcity.plugins import Plugins
 from numba import jit, cuda
 
 # %%
-# Plugins(categories=["generic", "privacy"]).list()
-# from sklearn.datasets import load_diabetes
-# X, y = load_diabetes(return_X_y=True, as_frame=True)
-# X["target"] = y
-# syn_model = Plugins().get("ctgan", n_iter=100, batch_size=50)
-# syn_model.fit(X)
-# new_data = syn_model.generate(count = len(X))
-# from synthcity.metrics import Metrics
-# Metrics().list()
-# Metrics().evaluate(X_gt=X, X_syn=new_data, metrics={'privacy':['k-anonymization', 'distinct l-diversity', 'identifiability_score']})
-
 
 def aux_singleouts(key_vars, dt):
     """create single out variable based on k-anonymity"""
     k = dt.groupby(key_vars)[key_vars[0]].transform(len)
-    dt['single_out'] = np.where(k < 2 , 1, 0)
+    dt['single_out'] = np.where(k < 3 , 1, 0)
     return dt
 
 def synth_city(msg, args):
@@ -40,11 +29,11 @@ def synth_city(msg, args):
     """
     cuda.select_device(int(args.id))
     print(msg)
-    output_interpolation_folder = 'data/synthcityk2/'
+    output_interpolation_folder = 'data/synthcity/'
 
     f = list(map(int, re.findall(r'\d+', msg.split('_')[0])))
     data = pd.read_csv(f'data/original/{str(f[0])}.csv')
-
+    print(f)
     # get 80% of data to synthesise
     indexes = np.load('indexes.npy', allow_pickle=True).item()
     indexes = pd.DataFrame.from_dict(indexes)
@@ -64,7 +53,14 @@ def synth_city(msg, args):
     
     keys_nr = list(map(int, re.findall(r'\d+', msg.split('_')[2])))[0]
     keys = set_key_vars[keys_nr]
+    if f[0] == 37: # because list of key vars have the original names (before the change due to SDV)
+        keys[keys.index("code_number")] = "state"
+        keys[keys.index("phone_number")] = "number"
+        keys[keys.index("voice_mail_plan")] = "voice_plan"
 
+    if f[0] == 55:
+        keys[keys.index("code_number")] = "state"
+    print(keys)
     data = aux_singleouts(keys, data)
     protected_data = data.loc[data['single_out'] == 0].reset_index(drop=True)
     unprotected_data = data.loc[data['single_out'] == 1].reset_index(drop=True)
@@ -83,11 +79,7 @@ def synth_city(msg, args):
         epi = list(map(float, re.findall(r'\d+\.\d+', msg.split('_')[5])))[0]
         model = Plugins().get("pategan", n_iter=epo, batch_size=bs, epsilon=epi)
 
-    elif technique=='privbayes':
-        epi = list(map(float, re.findall(r'\d+\.\d+', msg.split('_')[3])))[0]
-        model = Plugins().get("privbayes", epsilon=epi)
-    
-    # try:
+
     new_data = modeling(model, unprotected_data)
     new_data_ = pd.concat([new_data, protected_data])
 
@@ -95,10 +87,6 @@ def synth_city(msg, args):
     new_data_.to_csv(
         f'{output_interpolation_folder}{sep}{msg}.csv',
         index=False)
-    # except Exception:
-    #     with open('output/failed_file_synth.txt', 'a') as failed_file:
-    #         #  Save the name of the failed file to a text file
-    #         failed_file.write(f'{msg} --- city\n')
 
 # function optimized to run on gpu 
 @jit(target_backend='cuda')

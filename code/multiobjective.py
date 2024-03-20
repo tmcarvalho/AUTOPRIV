cc9733891227c5d72f08411fe2608eaf979a7f7f
tmcarvalho <taniammc.94@gmail.com>
2024-03-20 14:37:25 +0000

    add rank predictions to the plot

--- code/multiobjective.py
+++ code/multiobjective.py
@@ -87,7 +87,7 @@ pareto_front_df = pd.DataFrame(pareto_front)
 # Create a scatter plot for Pareto front
 sns.set_style("darkgrid")
 plt.figure(figsize=(11,7))
-sns.scatterplot(data=predictions, x="Predictions Performance", y="Predictions Linkability", color='#0083FF', s=70, label='All Solutions', alpha=0.7)
+sns.scatterplot(data=predictions, x="Predictions Performance", y="Predictions Linkability", color='#0083FF', s=70, label='All Solutions', alpha=0.65)
 sns.regplot(data=pareto_front_df, x="Predictions Performance", y="Predictions Linkability", color='red', label='Pareto Front')
 sns.set(font_scale=1.5)
 plt.xlabel('Performance Predictions')
@@ -95,9 +95,21 @@ plt.ylabel('Linkability Predictions')
 # plt.title('Pareto Front Analysis')
 plt.legend()
 plt.grid(True)
-# plt.show()
 plt.savefig(f'{os.path.dirname(os.getcwd())}/output_analysis/plots/pareto.pdf', bbox_inches='tight')
 
 #plt.xticks(np.arange(min(predictions['Predictions Performance']), max(predictions['Predictions Performance'])+0.001, 0.001))
 #plt.gca().xaxis.set_major_formatter(plt.FormatStrFormatter('%.3f'))  # Adjust decimal places as needed
+# %%
+pareto_rank = predictions.head(15)
+sns.set_style("darkgrid")
+plt.figure(figsize=(11,7))
+sns.scatterplot(data=predictions, x="Predictions Performance", y="Predictions Linkability", color='#0083FF', s=70, label='All Solutions', alpha=0.65)
+sns.regplot(data=pareto_rank, x="Predictions Performance", y="Predictions Linkability", color='red', label='Best Rank')
+sns.set(font_scale=1.5)
+plt.xlabel('Performance Predictions')
+plt.ylabel('Linkability Predictions')
+plt.legend()
+plt.grid(True)
+# plt.savefig(f'{os.path.dirname(os.getcwd())}/output_analysis/plots/pareto_rank.pdf', bbox_inches='tight')
+
 # %%
